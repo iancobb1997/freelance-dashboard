d151a61e9416c89fc84059ed7e3dd1242c1a183d
Ian Cobb <iancobb@Ians-MacBook-Pro.local>
2017-10-18 16:42:40 -0700

    added documents component

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -14,6 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
 	declarations: [
 		AppComponent,
 		HomepageComponent
+		DocumentsComponent
 	],
 	bootstrap: [
 		AppComponent
