81a1a218c8f45aab029caf2b1bb35f9e62000901
Ian Cobb <iancobb@Ians-MacBook-Pro.local>
2017-10-19 09:41:14 -0700

    Populated angular list view with data

--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
 	moduleId: module.id,
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://portfolio.iancobb.com', 'Ruby on Rails', 150, 120, 15, 'ian@devcamp.com')
+	proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://portfolio.iancobb.com', 'Ruby on Rails', 150, 120, 15, 'ian@devcamp.com')
+	proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://portfolio.iancobb.com', 'Ruby on Rails', 150, 120, 15, 'ian@devcamp.com')
+
+	proposals: Proposal[] = [
+		this.proposalOne, 
+		this.proposalTwo, 
+		this.proposalThree
+	]
+}
