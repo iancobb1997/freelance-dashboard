317b3ef7c807401b004b910953fbe7c2a4e35551
Ian Cobb <iancobb@Ians-MacBook-Pro.local>
2017-10-19 22:33:08 -0700

    Implemented bootstrap 4

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
+		NgbModule.forRoot()
 	],
 	declarations: [
 		AppComponent,
@@ -22,6 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
 		DocumentsComponent,
 		ProposalListComponent,
 		ProposalNewComponent,
+		ProposalShowComponent,
 	],
 	bootstrap: [
 		AppComponent
